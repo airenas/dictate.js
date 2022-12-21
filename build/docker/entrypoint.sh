4255e22717f1b28cfb8fa13921db835d7943021b
Airenas Vaičiūnas <airenass@gmail.com>
2022-12-21 13:43:51 +0200

    Fix log

--- build/docker/entrypoint.sh
+++ build/docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-echo Updating base href to ${SERVER}
+echo Updating server to: ${SERVER}
+echo Updating base href to: ${BASE_HREF}
 cat /usr/share/nginx/html/demos/mob.html.in | envsubst '${SERVER} ${BASE_HREF}' > /usr/share/nginx/html/demos/mob.html
 echo Done. Lets start nginx...
